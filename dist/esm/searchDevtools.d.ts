938ffdbaf806926e9765f752cfb93da7ddfdd42b
sprout2000 <wassabie64@gmail.com>
2021-07-03 14:01:14 +0900

    docs: Updated the homepage section.

--- dist/esm/searchDevtools.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type Devtools = 'JQUERY' | 'ANGULAR' | 'VUE' | 'VUE3' | 'REACT' | 'REDUX';
-export declare const whichDevtools: (arg: Devtools) => string;
-export declare const getExtDir: (platform: string) => string;
-export declare const searchDevtools: (arg: Devtools) => Promise<string | void>;
-//# sourceMappingURL=searchDevtools.d.ts.map
