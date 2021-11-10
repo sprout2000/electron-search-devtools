6ca0a350440cb5284e075c917ea0fabc92441d56
sprout2000 <wassabie64@gmail.com>
2021-11-11 08:46:12 +0900

    fix: Fixed prepare-test.sh for Linux.

--- prepare-test.sh
+++ prepare-test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "$(uname)" == 'Darwin' ]; then
+if [ "$(uname)" = 'Darwin' ]; then
   OS='darwin'
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
   OS='linux'
-elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW64_NT' ]; then                                                                                           
+elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW64_NT' ]; then                                                                                           
   OS='win32'
 else
   echo "Your platform ($(uname -a)) is not supported."
@@ -14,13 +14,13 @@ fi
 VUE='Default/Extensions/ljjemllljcmogpfapbkkighbhhppjdbg'
 VUE3='Default/Extensions/nhdogjmejiglipccpnnnanhbledajbpd/10.0_1'
 
-if [ ${OS} == 'darwin' ]; then
+if [ ${OS} = 'darwin' ]; then
   mkdir -p ~/Library/Application\ Support/Google/Chrome/${VUE}
   mkdir -p ~/Library/Application\ Support/Google/Chrome/${VUE3}
-elif [ ${OS} == 'linux' ]; then
+elif [ ${OS} = 'linux' ]; then
   mkdir -p ~/.config/google-chrome/${VUE}
   mkdir -p ~/.config/google-chrome/${VUE3}
-elif [ ${OS} == 'win32' ]; then
+elif [ ${OS} = 'win32' ]; then
   mkdir -p ~/AppData/Local/Google/Chrome/User\ Data/${VUE}
   mkdir -p ~/AppData/Local/Google/Chrome/User\ Data/${VUE3}
 else
