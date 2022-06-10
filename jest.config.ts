a93fd0a41f5a0e322efad34c7bd13a10263dd645
sprout2000 <wassabie64@gmail.com>
2022-06-11 07:56:28 +0900

    chore: Added coverageReporters.

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,7 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testMatch: ['<rootDir>/src/**/*.test.ts'],
   collectCoverage: true,
-  errorOnDeprecated: true,
-  testEnvironment: 'node',
+  coverageReporters: ['json-summary', 'text'],
 };
 
 export default config;
